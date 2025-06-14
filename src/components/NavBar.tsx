30e9cbc3e9e8a0deed52ee7e2edc6cb98fa1527b
LTowner <LTowner622@gmail.com>
2025-06-14 13:57:11 -0400

    Remove unused React import from NavBar.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { MoonIcon, SunIcon, Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 
